a43cf2c8677674dd21096cdee4ca7e2c3c894bda
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2013-02-04 12:22:04 -0500

    Added pkgs subdirectory.

--- matlab/startup.m
+++ matlab/startup.m
@@ -1,9 +1,19 @@
 % startup script.
-if exists('../startup_ahf.m', 'file')
-   run('../../startup_ahf.m');
+if exist('../startup_ahf.m', 'file')
+   run('../startup_ahf.m');
 end
-addpath([projroot 'matlab/1d'])
-addpath([projroot 'matlab/2d'])
-addpath([projroot 'matlab/3d'])
-addpath([projroot 'matlab/mixed'])
+addpath([projroot '/matlab/low/1d']) %#ok<*MCAP>
+addpath([projroot '/matlab/low/2d'])
+addpath([projroot '/matlab/low/3d'])
+addpath([projroot '/matlab/low/mixed'])
+addpath([projroot '/matlab/mid/1d'])
+addpath([projroot '/matlab/mid/2d'])
+addpath([projroot '/matlab/mid/3d'])
+addpath([projroot '/matlab/mid/mixed'])
+addpath([projroot '/matlab/hi/1d'])
+addpath([projroot '/matlab/hi/2d'])
+addpath([projroot '/matlab/hi/3d'])
+addpath([projroot '/matlab/hi/mixed'])
+addpath([projroot '/matlab/qual'])
 addpath('test')
+addpath('ex')
