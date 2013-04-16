5a6b38527f468fc7b04800eeec565ed492337477
Vladimir84 <vladimir.dedov@gmail.com>
2013-04-16 01:19:40 -0400

    minor fixes to reader

--- matlab/IO/Meshes/readvtk_cst.m
+++ matlab/IO/Meshes/readvtk_cst.m
@@ -2,7 +2,7 @@ function [xs,cells,var_nodes,var_elems]=readvtk_cst(fname)
 
 [xs,elems,types,var_nodes,var_elems]=readvtk_cst_core(fname);
 p=0;
-types_num2str={'verteces','poly_verteces','edges','poly_lines','tris','strips','polygons','pixels','quads','tets','voxels','hexes','prisms','pyrs'};
+types_num2str={'vertices','poly_vertices','edges','poly_lines','tris','strips','polygons','pixels','quads','tets','voxels','hexes','prisms','pyrs'};
 types_num2ncoords=[2,0,3,0,4,0,0,5,5,5,9,9,7,6];
 cells=struct;
 
