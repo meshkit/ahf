d5242e028210154efb6e5091d9eb536feaa8b392
Vladimir84 <vladimir.dedov@gmail.com>
2013-04-22 00:10:23 -0500

    face to half-face mapping and test

---
+++ matlab/hi/mixed/f2hf.m
@@ -0,0 +1,82 @@
+function [hf, etags] = f2hf(fid,faces,tets,sibhfs, v2hf, etags)
+% For every face, obtain adjacent cells
+vid1=faces(fid,1);
+vid2=faces(fid,2);
+vid3=faces(fid,3);
+[found,fid,lvid1,lvid2,lvid3, etags] = examine_1ring_elems_tet_hf( vid1, vid2, vid3, tets, sibhfs, v2hf, etags);
+hf=0;
+if (found)
+    hf=match_halfface(fid,lvid1,lvid2,lvid3);
+end
+
+
+function hf = match_halfface(fid,lvid1,lvid2,lvid3)
+%hf_tet    = int32([1 3 2; 1 2 4; 2 3 4; 3 1 4]);
+
+% it happens that we can uniquely match triplet [lvid1,lvid2,lvid3] to
+% local face id using sum of local ids (for tet)
+
+% sum = 6 -> lfid = 1
+% sum = 7 -> lfid = 2
+% sum = 8 -> lfid = 4
+% sum = 9 -> lfid = 3
+lfid_map=[1,2,4,3];
+lfid=lfid_map(lvid1+lvid2+lvid3-5);
+hf=clfids2hfid(fid,lfid);
+
+
+
+function [found,eid,lvid1,lvid2,lvid3, etags] = examine_1ring_elems_tet_hf(vid1, vid2, vid3, tets, sibhfs, v2hf, etags)
+%OBTAIN_1RING_ELEMS_TET Collects 1-ring neighbor elements of tet mesh.
+% [NGBES, NELEMS, ETAGS] = OBTAIN_1RING_ELEMS_TET( VID, ...
+%         TETS, SIBHFS, V2HF, NGBES, ETAGS)
+% Collects 1-ring neighbor elements of given vertex and saves them into
+% NGBES. At input, ETAGS must be set to false. It is reset to false
+% at output.
+%hf_tet    = int32([1 3 2; 1 2 4; 2 3 4; 3 1 4]);
+
+coder.extrinsic('warning');
+
+found = false;
+eid=0;
+lvid1 = int32(0); % Stores which vertex vid is within the tetrahedron.
+lvid2 = int32(0);
+lvid3 = int32(0);
+% Obtain incident tetrahedron of vid.
+eid = hfid2cid(v2hf(vid1));
+if ~eid; return; end
+
+sibhfs_tet = int32([1 2 4; 1 2 3; 1 3 4; 2 3 4]);
+
+MAXTETS=50;
+% Create a stack for storing tets and insert element itself into stack
+stack = nullcopy(zeros(MAXTETS,1, 'int32'));
+size_stack = int32(1); stack(1) = eid;
+
+while size_stack>0
+    % Pop the element from top of stack
+    eid = stack(size_stack); size_stack = size_stack-1;
+    etags(eid) = true;
+    for ii=1:4
+        v = tets(eid,ii);
+        if v==vid1; lvid1 = ii; end
+        if v==vid2; lvid2 = ii; end;
+        if v==vid3; lvid3 = ii; end;
+    end
+    
+    if (lvid2 && lvid3)
+        % found matching face
+        found = true;
+        etags(:) = false;
+        return;
+    end
+    
+    % Push unvisited neighbor tets onto stack
+    for ii=1:3
+        ngb = hfid2cid(sibhfs(eid,sibhfs_tet(lvid1,ii)));
+        if ngb && ~etags(ngb);
+            size_stack = size_stack + 1; stack(size_stack) = ngb;
+        end
+    end
+end
+etags(:) = false;
---
+++ matlab/hi/mixed/test/test_f2hf.m
@@ -0,0 +1,47 @@
+function passed = test_h2hf
+
+faces=[1,2,3;  % boundary face
+       2,3,4;  % interior face
+       5,1,4;  % face not embedded into the volume mesh
+       6,3,11; % vertices not in the volume mesh
+       ];
+
+% tetrahedral contains one interior face
+tets = [1,2,3,4;
+        2,3,4,5;
+       ];
+   
+[sibhfs,v2hf] = construct_halffaces( 11, tets);
+
+
+etags=false(size(tets,1),1);
+% order of vertices in tetrahedra
+% [1 3 2; 1 2 4; 2 3 4; 3 1 4]    
+
+%% Test 1: boundary face
+fid=1;
+[hf, etags] = f2hf(fid,faces,tets,sibhfs, v2hf, etags);
+passed=(hfid2cid(hf)==1) && (hfid2lfid(hf)==1);
+passed = passed && ~any(etags);
+
+%% Test 2: interior face
+fid=2;
+[hf, etags] = f2hf(fid,faces,tets,sibhfs, v2hf, etags);
+passed=passed && ((hfid2cid(hf)==2 && hfid2lfid(hf)==1) || (hfid2cid(hf)==1 && hfid2lfid(hf)==3));
+passed = passed && ~any(etags);
+
+%% Test 3: face not embedded into the volume mesh
+fid=3;
+[hf, etags] = f2hf(fid,faces,tets,sibhfs, v2hf, etags);
+passed=passed && hf==0;
+passed = passed && ~any(etags);
+
+%% Test 4: vertices not in the volume mesh
+fid=4;
+[hf, etags] = f2hf(fid,faces,tets,sibhfs, v2hf, etags);
+passed=passed && hf==0;
+passed = passed && ~any(etags);
+
+
+
+
