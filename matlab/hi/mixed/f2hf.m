1e1f55884c172bd960a995d0e5cddfbe47d78ff4
Vladimir84 <vladimir.dedov@gmail.com>
2013-05-12 21:44:02 -0500

    fixed some bugs

--- matlab/hi/mixed/f2hf.m
+++ matlab/hi/mixed/f2hf.m
@@ -1,5 +1,5 @@
-function [hf, etags] = f2hf(fid,faces,tets,sibhfs, v2hf, etags) %#codegen
-%#codegen -args {int32(0), coder.typeof(int32(0), [inf,3]),coder.typeof(int32(0), [inf,4]),coder.typeof(int32(0), [inf,4]),coder.typeof(int32(0), [inf,1]),coder.typeof(false, [inf,4])}
+function [fid, lfid, etags] = f2hf(fid,faces,tets,sibhfs, v2hf, etags) %#codegen
+%#codegen -args {int32(0), coder.typeof(int32(0), [inf,3]),coder.typeof(int32(0), [inf,4]),coder.typeof(int32(0), [inf,4]),coder.typeof(int32(0), [inf,1]),coder.typeof(false, [inf,1])}
 %%%#codegen determine_sibling_halffaces_usestruct -args
 %%%#codegen {int32(0), coder.typeof(int32(0), [inf,inf]), false}
 
@@ -11,15 +11,19 @@ vid2=faces(fid,2);
 vid3=faces(fid,3);
 [found,fid,lvid1,lvid2,lvid3, etags] = examine_1ring_elems_tet_hf( vid1, vid2, vid3, tets, sibhfs, v2hf, etags);
 hf=int32(0);
+
 for i = 1 : size(etags,1)
     etags(i,1)=false;
 end
 if (found)
-    hf=match_halfface(fid,lvid1,lvid2,lvid3);
+    [fid, lfid]=match_halfface(fid,lvid1,lvid2,lvid3);
+else
+    fid=int32(0);
+    lfid=int32(0);
 end
 
 
-function hf = match_halfface(fid,lvid1,lvid2,lvid3)
+function [fid, lfid] = match_halfface(fid,lvid1,lvid2,lvid3)
 %hf_tet    = int32([1 3 2; 1 2 4; 2 3 4; 3 1 4]);
 
 % it happens that we can uniquely match triplet [lvid1,lvid2,lvid3] to
@@ -29,7 +33,7 @@ function hf = match_halfface(fid,lvid1,lvid2,lvid3)
 % sum = 7 -> lfid = 2
 % sum = 8 -> lfid = 4
 % sum = 9 -> lfid = 3
-lfid_map=[1,2,4,3];
+lfid_map=int32([1,2,4,3]);
 lfid=lfid_map(lvid1+lvid2+lvid3-5);
 hf=clfids2hfid(fid,lfid);
 
@@ -48,11 +52,10 @@ coder.extrinsic('warning');
 
 found = false;
 eid=0;
-lvid1 = int32(0); % Stores which vertex vid is within the tetrahedron.
-lvid2 = int32(0);
-lvid3 = int32(0);
 % Obtain incident tetrahedron of vid.
 eid = hfid2cid(v2hf(vid1));
+
+lvid1 = int32(0); lvid2 = int32(0); lvid3 = int32(0);
 if ~eid; return; end
 
 sibhfs_tet = int32([1 2 4; 1 2 3; 1 3 4; 2 3 4]);
@@ -66,6 +69,11 @@ while size_stack>0
     % Pop the element from top of stack
     eid = stack(size_stack); size_stack = size_stack-1;
     etags(eid) = true;
+    
+    lvid1 = int32(0); % Stores which vertex vid is within the tetrahedron.
+    lvid2 = int32(0);
+    lvid3 = int32(0);
+   
     for ii=int32(1):4
         v = tets(eid,ii);
         if v==vid1; lvid1 = ii; end
@@ -73,7 +81,10 @@ while size_stack>0
         if v==vid3; lvid3 = ii; end;
     end
     
-    if (lvid2 && lvid3)
+    if (lvid1 && lvid2 && lvid3)
+        if lvid1==lvid2 || lvid1==lvid3 || lvid2==lvid3
+            %fprintf('?\n');
+        end
         % found matching face
         found = true;
         etags(stack(1:size_stack,1),1) = false;
