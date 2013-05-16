edc3fc2c570613e14b7f770e9cf56d27943eb1cd
Vladimir84 <vladimir.dedov@gmail.com>
2013-05-15 23:46:09 -0500

    functions working with half-vertices modified to work with struct input and be compiled

--- matlab/hi/1d/determine_incident_halfverts.m
+++ matlab/hi/1d/determine_incident_halfverts.m
@@ -13,23 +13,34 @@ function v2hv = determine_incident_halfverts(nv, edgs, varargin)
 % See also DETERMINE_SIBLING_HALFVERTS
 
 %#codegen -args {int32(0), coder.typeof( int32(0), [inf, 2])}
-
+%#codegen determine_incident_halfverts_usestruct -args {int32(0), coder.typeof( int32(0), [inf, 2]),false}
 nedgs = int32(size(edgs,1));
 
 % Construct a vertex to halfedge mapping.
-if nargin<3 || isempty( varargin{1})
+if nargin<3 || isempty( varargin{1}) || ~islogical(varargin{1})
     v2hv = zeros( nv, 1, 'int32');
 else
-    v2hv = varargin{1};
-    v2hv(:) = 0;
+    v2hv = struct('eid',zeros( nv, 1, 'int32'),'lvid',zeros( nv, 1, 'int8'));
 end
 
 % Compute v2hv.
-for kk=1:nedgs
-    for lid=1:2
-        v = edgs(kk,lid);
-        if v>0 && v2hv(v)==0
-            v2hv(v) = elvids2hvid( kk, lid);
+if nargin<3 || isempty( varargin{1}) || ~islogical(varargin{1})
+    for kk=1:nedgs
+        for lid=1:2
+            v = edgs(kk,lid);
+            if v>0 && v2hv(v)==0
+                v2hv(v) = elvids2hvid( kk, lid);
+            end
+        end
+    end
+else
+    for kk=1:nedgs
+        for lid=1:2
+            v = edgs(kk,lid);
+            if v>0 && v2hv.eid(v)==0
+                v2hv.eid(v) = kk; 
+                v2hv.lvid(v)= lid;                
+            end
         end
     end
 end
---
+++ matlab/hi/1d/determine_sibling_halfverts_usestruct.m
@@ -0,0 +1,2 @@
+function [sibhvs,manifold,oriented] = determine_sibling_halfverts_usestruct(nv, edges, usestruct)
+[sibhvs,manifold,oriented] = determine_sibling_halfverts(nv, edges, usestruct);
