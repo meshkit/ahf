34c9beb7cda70007fc8279ec0b7a7f12ccdf89e5
Rebecca Conley <conley.rebecca@gmail.com>
2014-02-18 14:07:58 -0500

    Fixed bugs

--- matlab/mid/3d/obtain_nring_vol.m
+++ matlab/mid/3d/obtain_nring_vol.m
@@ -1,5 +1,6 @@
 function [ngbvs, nverts, vtags, etags, ngbes, nelems] = obtain_nring_vol( vid, ring, minpnts, ...
         tets, sibhfs, v2hf, ngbvs, vtags, etags, ngbes) %#codegen 
+    
 %OBTAIN_NRING_VOL Collect 1-ring neighbor vertics and elements.
 % [NGBVS,NVERTS,VTAGS,ETAGS,NGBES,NELEMS] = OBTAIN_NRING_VOL(VID,RING, ...
 % MINPNTS,TETS,SIBHFS,V2HF,NGBVS,VTAGS,ETAGS,NGBES) Collects 1-ring 
@@ -29,6 +30,13 @@ vtags(vid) = true;
 [ngbvs, nverts, vtags, etags, ngbes, nelems] = ...
     append_one_ring( vid, tets, sibhfs, v2hf, ngbvs, nverts, vtags, etags, ngbes, nelems);
 
+if ring == 1 && nverts>=minpnts
+    % Reset flags
+    vtags(vid) = false;
+    vtags(ngbvs(1:nverts)) = false; etags(ngbes(1:nelems)) = false;
+    return;
+end
+
 % Second, build full-size ring
 nverts_pre = int32(1);
 minpnts = min(minpnts, MAXNPNTS);
@@ -40,7 +48,7 @@ while 1
 
     for ii = nverts_pre+1 : nverts_last
         [ngbvs, nverts, vtags, etags, ngbes, nelems] = ...
-            append_one_ring( ngbvs(nverts), tets, sibhfs, v2hf, ngbvs, ...
+            append_one_ring( ngbvs(ii), tets, sibhfs, v2hf, ngbvs, ...
             nverts, vtags, etags, ngbes, nelems);
     end
 
@@ -68,31 +76,33 @@ stack = nullcopy(zeros(MAXNTETS,1,'int32'));
 size_stack = int32(1); stack(1) = rid;
 
 % Insert element itself into queue.
-sibhfs_tet = int32([1, 2, 4; 1 2 3; 1 3 4; 2 3 4]);
+sibhfs_tet = int32([1 2 4; 1 2 3; 1 3 4; 2 3 4]);
 while size_stack>0
     % Pop the element from top of stack
     rid = stack(size_stack); size_stack = size_stack-1;
-    etags(rid) = 1;
+    if ~etags(rid)
+        etags(rid) = 1;
 
-    % Append element
-    nelems = nelems + 1; ngbes(nelems) = rid;
+        % Append element
+        nelems = nelems + 1; ngbes(nelems) = rid;
 
-    lvid = int32(0); % Stores which vertex vid is within the tetrahedron.
-    % Append vertices
-    for ii=int32(1):4
-        v = tets(rid,ii);
-        if v == vid; lvid = ii; end
+        lvid = int32(0); % Stores which vertex vid is within the tetrahedron.
+        % Append vertices
+        for ii=int32(1):4
+            v = tets(rid,ii);
+            if v == vid; lvid = ii; end
 
-        if ~vtags( v)
-            vtags( v) = true; nverts = nverts + 1; ngbvs(nverts) = v;
+            if ~vtags( v)
+                vtags( v) = true; nverts = nverts + 1; ngbvs(nverts) = v;
+            end
         end
-    end
 
-    % Push unvisited neighbor tets onto stack
-    for ii=int32(1):3
-        ngb = hfid2cid(sibhfs(rid,sibhfs_tet(lvid,ii)));
-        if ngb && ~etags(ngb);
-            size_stack = size_stack + 1; stack(size_stack) = ngb;
+        % Push unvisited neighbor tets onto stack
+        for ii=int32(1):3
+            ngb = hfid2cid(sibhfs(rid,sibhfs_tet(lvid,ii)));
+            if ngb && ~etags(ngb);
+                size_stack = size_stack + 1; stack(size_stack) = ngb;
+            end
         end
     end
 end
